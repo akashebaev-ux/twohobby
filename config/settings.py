f3f8a988cf93991d42bca3cb3f8a33192c8a6167
akashebaev-ux <akashebaev@gmail.com>
2026-05-12 00:22:57 +0200

    Configure WhiteNoise static files for Heroku

--- config/settings.py
+++ config/settings.py
@@ -158,6 +158,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Redis config 
 
