a608c36359b9ebbfca5f7fcfc465689007bc65fd
akashebaev-ux <akashebaev@gmail.com>
2026-05-11 19:03:52 +0200

    Add JavaScript WebSocket client for real-time chat messaging

--- config/asgi.py
+++ config/asgi.py
@@ -12,7 +12,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from chatbox.routing import websocket_urlpatterns
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
