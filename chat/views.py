6716da2a28200f5568c2a0b935fce0390031e6ef
akashebaev-ux <akashebaev@gmail.com>
2026-05-11 19:45:12 +0200

    Add chat list and room rendering views

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+def chat_list(request):
+    return render(request, "chat/chat_list.html")
+
 def room(request, room_name):
     return render(request, "chat/room.html", {
         "room_name": room_name
