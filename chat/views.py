9bfae9f4b5fef5789b79d2d376bfa619fc99081f
akashebaev-ux <akashebaev@gmail.com>
2026-05-11 21:43:45 +0200

    Refactor real-time chat UI and improve messaging CSS styling

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import ChatRoom
 
