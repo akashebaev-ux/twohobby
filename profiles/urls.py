edcb5560af878d34ae7e12c123e003808db46067
akashebaev-ux <akashebaev@gmail.com>
2026-05-12 22:27:06 +0200

    Add edit profile route for user profile management

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.landing_page, name='landing'),
     path('nearby/', views.ProfileListView.as_view(), name='profile_list'),
+    path("profile/edit/", views.edit_profile, name="edit_profile"),
     path('profile/<int:id>/', views.profile_detail, name='profile_detail'),
     path("my-profile/", views.my_profile, name="my_profile"),
 ]
