eb03a842071d291968e981d8d74bb063b051bbf2
akashebaev-ux <akashebaev@gmail.com>
2026-05-14 21:14:18 +0200

    Add likes feed view with liked users posts and posting logic

--- matches/views.py
+++ matches/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, redirect
-
-from .models import Swipe
+from django.shortcuts import get_object_or_404, redirect, render
+from .forms import LikePostForm
+from .models import Swipe, LikePost
 
 
 @login_required
@@ -17,3 +17,38 @@ def swipe_user(request, user_id, action):
         )
 
     return redirect("encounters")
+
+
+@login_required
+def likes(request):
+    liked_users = Swipe.objects.filter(
+        from_user=request.user,
+        action="like"
+    ).select_related("to_user")
+
+    liked_user_ids = liked_users.values_list("to_user", flat=True)
+
+    posts = LikePost.objects.filter(
+        author__id__in=list(liked_user_ids) + [request.user.id]
+    )
+
+    can_post = liked_users.exists()
+
+    if request.method == "POST" and can_post:
+        form = LikePostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+
+            return redirect("likes")
+    else:
+        form = LikePostForm()
+
+    return render(request, "matches/likes.html", {
+        "liked_users": liked_users,
+        "posts": posts,
+        "form": form,
+        "can_post": can_post,
+    })
