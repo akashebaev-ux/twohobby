df1a56704335c8f4de635354e8e700bbd8811e38
akashebaev-ux <akashebaev@gmail.com>
2026-05-16 07:28:49 +0200

    Add dynamic target language selection for realtime translation

--- static/js/translate.js
+++ static/js/translate.js
@@ -1,9 +1,11 @@
 window.translateText =
-async function(text) {
+async function(text, targetLanguage = "en") {
 
     const response = await fetch(
-        "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q="
-        + encodeURIComponent(text)
+        "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl="
+        + targetLanguage +
+        "&dt=t&q=" +
+        encodeURIComponent(text)
     );
 
     const data = await response.json();
