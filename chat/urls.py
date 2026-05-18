4851db2aac639e3c6e40e2f01eddc13961060ec7
akashebaev-ux <akashebaev@gmail.com>
2026-05-18 18:51:33 +0200

    Add route for chat image uploads for the Webp format

--- chat/urls.py
+++ chat/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path("<int:room_id>/", views.room, name="room"),
     path("start/<int:user_id>/", views.start_chat, name="start_chat"),
     path("delete/<int:room_id>/", views.delete_chat, name="delete_chat"),
+    path(
+        "room/<int:room_id>/upload-image/",
+        views.upload_chat_image,
+        name="upload_chat_image"
+    ),
 ]
