97424d42cf39d11aef6fa7190ab6de40e1fb5737
akashebaev-ux <akashebaev@gmail.com>
2026-05-18 21:30:38 +0200

    Add Channels imports for chat image broadcasting

--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, redirect, render
 from matches.models import BlockedUser
 from .models import ChatRoom, ChatMessage
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 
 
 @login_required
