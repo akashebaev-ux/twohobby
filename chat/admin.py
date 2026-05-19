3f01eb6ab5e26e271e9f5f040a0f5550f268aeb7
akashebaev-ux <akashebaev@gmail.com>
2026-05-19 22:34:49 +0200

    Fix admin imports for chat

--- chat/admin.py
+++ chat/admin.py
@@ -1,43 +1,9 @@
 from django.contrib import admin
 
 from .models import (
-    Profile,
     ChatRoom,
     ChatMessage,
 )
-
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-
-    list_display = (
-        "display_name",
-        "age",
-        "gender",
-        "looking_for",
-        "location",
-        "is_active",
-    )
-
-    search_fields = (
-        "display_name",
-        "location",
-        "bio",
-    )
-
-    list_filter = (
-        "gender",
-        "looking_for",
-        "is_active",
-    )
-
-    list_editable = (
-        "is_active",
-    )
-
-    ordering = (
-        "-created_on",
-    )
 
 
 @admin.register(ChatRoom)
