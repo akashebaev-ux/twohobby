310d9abc461e787fb094b5fcd090675092ec9a40
akashebaev-ux <akashebaev@gmail.com>
2026-05-22 07:02:24 +0200

    Configure Django settings for Heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -171,7 +171,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CLOUDINARY_STORAGE = {
