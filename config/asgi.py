25d14fa3457d190682f5c173c945492b3d872c15
akashebaev-ux <akashebaev@gmail.com>
2026-05-22 07:54:09 +0200

    Fix ASGI app registry initialization

--- config/asgi.py
+++ config/asgi.py
@@ -8,21 +8,21 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+
+from django.core.asgi import get_asgi_application  # noqa
+
+django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack  # noqa
 from channels.routing import ProtocolTypeRouter, URLRouter  # noqa
-from django.core.asgi import get_asgi_application  # noqa
 from chat.routing import websocket_urlpatterns  # noqa
-
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
