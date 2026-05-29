540f2fe12fb0e110c480aa30f18f098f228a15c4
akashebaev-ux <akashebaev@gmail.com>
2026-05-29 22:44:16 +0200

    Improve filter responsiveness and validate age inputs

--- static/js/nearby.js
+++ static/js/nearby.js
@@ -39,3 +39,40 @@ if (filterBtn && filterBox) {
         }
     );
 }
+
+const minAgeInput =
+    document.querySelector("input[name='min_age']");
+
+const maxAgeInput =
+    document.querySelector("input[name='max_age']");
+
+function validateAgeInput(input) {
+
+    input.addEventListener("input", function() {
+
+        this.value =
+            this.value.replace(/[^0-9]/g, "");
+
+        const age =
+            Number(this.value);
+
+        if (this.value && (age < 18 || age > 100)) {
+
+            this.setCustomValidity(
+                "Age must be between 18 and 100."
+            );
+
+        } else {
+
+            this.setCustomValidity("");
+        }
+    });
+}
+
+if (minAgeInput) {
+    validateAgeInput(minAgeInput);
+}
+
+if (maxAgeInput) {
+    validateAgeInput(maxAgeInput);
+}
