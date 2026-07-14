1eb77f23d4c07e60322a572a11dcf1ac61fc54b4
akashebaev-ux <akashebaev@gmail.com>
2026-07-14 17:50:40 +0200

    Add authenticated rides page

--- rides/views.py
+++ rides/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+
+
+@login_required
+def ride_list(request):
+    return render(request, "rides/ride_list.html")
