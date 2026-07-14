6ce46701a1bd88ed977b3284bf4b4b53a2d61e7e
akashebaev-ux <akashebaev@gmail.com>
2026-07-14 18:08:21 +0200

    Add rides to Django admin

--- rides/admin.py
+++ rides/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Ride
 
 # Register your models here.
+
+
+@admin.register(Ride)
+class RideAdmin(admin.ModelAdmin):
+    list_display = (
+        "driver",
+        "start_name",
+        "destination_name",
+        "departure_time",
+        "available_seats",
+        "status",
+    )
+
+    list_filter = (
+        "status",
+        "departure_time",
+    )
+
+    search_fields = (
+        "driver__username",
+        "start_name",
+        "destination_name",
+    )
